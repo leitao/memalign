293cdf3b939592671f700de719513c3eb52b1b7e
Breno Leitao <breno.leitao@gmail.com>
2017-04-30 19:35:58 -0300

    Improve output

--- align.c
+++ align.c
@@ -6,13 +6,17 @@ int main(int argc, char **argv){
 
 	for (b = 0; b < sizeof(align)/sizeof(int) ; b++) {
 		printf("\n====================================\n");
-		printf("Memory allocationg aligned in %d\n", align[b]);
+		printf("Memory allocating aligned in %d", align[b]);
+		if (align[b] == 0)
+			printf(" (Default malloc)\n");
+		else
+			printf("\n");
 		printf("====================================\n");
 		for (shift = 0 ; shift < 8 ; shift++){
 			if (shift == 0)
 				printf("\nAligned access\n");
 			else
-				printf("\nUn-aligned in +%d bytes\n", shift);
+				printf("\nUn-aligned in +%d bytes access\n", shift);
 
 			default_allocation8(shift, align[b]);
 			default_allocation16(shift, align[b]);
